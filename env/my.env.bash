4b94ecfa08bd3cef8f892fffddbda58b7f36a1f6
root <root@dev.com>
2022-01-24 23:50:58 +0800

    edit env

--- env/my.env.bash
+++ env/my.env.bash
@@ -4,10 +4,10 @@
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LD_LIBRARY_PATH="/usr/local/instantclient"
-export APACHE="/usr/local/apache"
-export PHP="/usr/local/php"
-export PATH="${HOME}/bin:${HOME}/.g/go/bin:$PATH:$APACHE/bin:$PHP/bin"
-
+export APACHE="/usr/local/apache/bin"
+export PHP="/usr/local/php/bin"
+export HOME_BIN="$HOME/bin"
+export PATH=$PATH:$PHP:$HOME_BIN:$APACHE
 
 # bind
 # bind 'set show-all-if-ambiguous on'
