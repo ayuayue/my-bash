81b59a78690e4c97c4281006418787994702f7c4
ayuayue <1401262639@qq.com>
2022-01-28 14:10:21 +0800

    屏蔽无错误时输出

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ ALIAS_DIR=$BASH_DIR/alias
 PLUGIN_DIR=$BASH_DIR/plugins
 
 checkEnv() {
-echo '初始化 my-bash 检查运行环境 ...'
+echo '初始化 my-bash 检查运行环境 ...' &> /dev/null
 if [ $SHELL != '/bin/bash' ]; then
      echo "默认shell不是/bin/bash,运行结束"
      exit
@@ -30,30 +30,30 @@ load_file(){
   for file in $ENABLE_DIR/*; do
     source $file
 done
-echo '初始化文件完成'
+echo '初始化文件完成' &> /dev/null
 }
 
 add_bashrc(){
 
 if [ `grep -c "source $BASH_DIR/init.sh" $BASHRC` -ne '0' ];then
-    echo "配置文件已引入,跳过"
+    echo "配置文件已引入,跳过" &> /dev/null
 else
     echo "source $BASH_DIR/init.sh" >> $BASHRC
-    echo "配置引入完成,请执行 source $BASHRC 更新bashrc文件"
+    echo "配置引入完成,请执行 source $BASHRC 更新bashrc文件" &> /dev/null
 fi
 
 if [ `grep -c "source $BASH_DIR/theme/theme.bash" $BASHRC` -ne '0' ];then
-    echo "主题配置文件已引入,跳过"
+    echo "主题配置文件已引入,跳过" &> /dev/null
 else
     echo "source $BASH_DIR/theme/theme.bash" >> $BASHRC
-    echo "主题配置引入完成,请执行 source $BASHRC 更新bashrc文件"
+    echo "主题配置引入完成,请执行 source $BASHRC 更新bashrc文件" &> /dev/null
 fi
 
 if [ `grep -c "source $BASH_DIR/env/my.env.bash" $BASHRC` -ne '0' ];then
-    echo "环境变量配置文件已引入,跳过"
+    echo "环境变量配置文件已引入,跳过" &> /dev/null
 else
     echo "source $BASH_DIR/env/my.env.bash" >> $BASHRC
-    echo "环境变量配置引入完成,请执行 source $BASHRC 更新bashrc文件"
+    echo "环境变量配置引入完成,请执行 source $BASHRC 更新bashrc文件" &> /dev/null
 fi
 }
 
@@ -66,7 +66,7 @@ bash_init() {
 }
 
 if [ "$(ls -A $ENABLE_DIR)" ]; then
-     echo "$ENABLE_DIR 不为空,即将清空并重新生成文件"
+     echo "$ENABLE_DIR 不为空,即将清空并重新生成文件" &> /dev/null
      rm -f $ENABLE_DIR/*
      bash_init
 else
