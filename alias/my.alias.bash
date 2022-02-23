1cfc1b9c6b2cd1484a3e80972b732b5eed5ff5d6
ayuayue <1401262639@qq.com>
2022-02-23 09:57:59 +0800

    edit proxy

--- alias/my.alias.bash
+++ alias/my.alias.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-export HTTP_PROXY=http://192.168.190.1:7890
-export HTTPS_PROXY=http://192.168.190.1:7890
-export NO_PROXY=http://127.0.0.1
+HTTP_PROXY=http://192.168.190.1:7890
+HTTPS_PROXY=http://192.168.190.1:7890
+NO_PROXY=http://127.0.0.1
 
 alias rm='rm -i'
 alias cp='cp -i'
@@ -9,9 +9,9 @@ alias mv='mv -i'
 alias l='ls -alh'
 alias ll='ls -alh'
 
-alias proxy="export http_proxy=$HTTP_PROXY && export https_proxy=$HTTPS_PROXY"
-alias noproxy="export http_proxy=$NO_PROXY && export https_proxy=$NO_PROXY"
-alias unproxy="unset $http_proxy && unset $https_proxy"
+alias proxy="export HTTP_PROXY=$HTTP_PROXY && export HTTPS_PROXY=$HTTPS_PROXY"
+alias noproxy="export HTTP_PROXY=$NO_PROXY && export HTTPS_PROXY=$NO_PROXY"
+alias unproxy="unset $HTTP_PROXY && unset $HTTPS_PROXY"
 
 # dnmp alias
 alias dnginx='docker exec -it nginx /bin/sh'
