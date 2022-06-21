da4baa7afdb3beae045827ba2f674af52024dd43
ayuayue <1401262639@qq.com>
2022-06-22 01:13:48 +0800

    常用开发软件及库安装

---
+++ plugins/tools.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# 常用开发工具安装
+
+yum groups mark convert
+
+yum grouplist 
+
+yum groupinstall "System Management" "Development Tools" -y
+
+yum install -y net-tools libxml2 libxml2-dev sqlite sqlite-devel gcc g++ psmisc vim tcpdump perf top autoconf valgrind expat-devel  glibc-headers gcc-c++ screenfetch tmux screen lrzsz pcre pcre-devel zlib zlib-devel ruby openssl openssl-devel patch wget bash-completion lsof unzip zip gdb make tree nmap dos2unix nc htop iftop iotop sysstat nethogs vim-enhanced epel-release 
