5c1806c788d3b85c644d7c4c4519121953f13bf1
ayuayue <1401262639@qq.com>
2022-10-24 16:00:10 +0800

    zsh

--- plugins/c_system_init.sh
+++ plugins/c_system_init.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env sh
 # Centos 系统初始化 
-# 安装 zsh oh-my-zsh 及插件，默认启动 zsh
-# 设置编码集
+
+yum install glibc-common langpacks-zh_CN git wget gcc g++ make automake lrzsz -y
+
+yum install curses curses-devel ncurses ncurses-devel  -y
+rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+yum install epel-release -y
+yum install yum-utils -y
+
 # 设置 docker 源
-# 时间同步
-
-yum install glibc-common -y
-yum install -y langpacks-zh_CN
-yum install git wget -y
-echo 'export LC_ALL="zh_CN.UTF-8"' >> /etc/profile
-source /etc/profile
-echo 'LANG="zh_CN.UTF-8"' >> /etc/locale.conf
-source /etc/locale.conf
-
 mkdir -p /etc/docker
 tee /etc/docker/daemon.json <<-'EOF'
 {
@@ -30,32 +26,40 @@ tee /etc/docker/daemon.json <<-'EOF'
   "experimental" : true
 }
 EOF
-systemctl daemon-reload
-systemctl restart docker
 
+
+# 时间同步
+yum -y install ntp
+ntpdate pool.ntp.org
+systemctl start ntpd
+
+# 设置编码集
+yum -y groupinstall chinese-support
+yum -y install chinese-support
+yum -y install kde-l10n-Chinese
+yum -y install ibus-table-chinese-1.4.6-3.el7.noarch
+
+
+echo 'LANG="zh_CN.UTF-8"' > /etc/locale.conf
+echo 'LANG="zh_CN.UTF-8"' > /etc/sysconfig/i18n
+source /etc/locale.conf
+source /etc/sysconfig/i18n
+export LANG="zh_CN.UTF-8"
+
+# 安装 zsh oh-my-zsh 及插件，默认启动 zsh
 cd ~
 wget https://jaist.dl.sourceforge.net/project/zsh/zsh/5.8/zsh-5.8.tar.xz --no-check-certificate
 tar xvf zsh-5.8.tar.xz
 cd zsh-5.8 && ./configure && make && make install
 echo /usr/local/bin/zsh >> /etc/shells
 chsh -s /usr/local/bin/zsh
+cd ~
 
-sh -c "$(curl -fsSL https://gitee.com/Devkings/oh_my_zsh_install/raw/master/install.sh)"
+sh -c "$(curl -fsSL https://gitee.com/caoayu/my-bash/raw/master/plugins/oh-my-zsh_install.sh)"
+
 
 git clone https://gitee.com/skarner/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 git clone https://gitee.com/Annihilater/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
-yum install epel-release -y
-yum install yum-utils -y
-
-
-
-yum -y install ntp
-ntpdate pool.ntp.org
-systemctl start ntpd
-
-
-yum update
-
+echo "omz plugin enable docker docker-compose yum systemd rust python npm supervisor yarn pip pm2 node man aliases command-not-found history firewalld golang composer cp " >> ~/.zshrc
