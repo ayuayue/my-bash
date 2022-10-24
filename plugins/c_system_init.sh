9bba72529a8592562808c794cc362c47fb1cb55b
ayuayue <1401262639@qq.com>
2022-10-24 16:17:53 +0800

    zsh

--- plugins/c_system_init.sh
+++ plugins/c_system_init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 # Centos 系统初始化 
 
-yum install glibc-common langpacks-zh_CN git wget gcc g++ make automake lrzsz -y
+yum install glibc-common langpacks-zh_CN git wget gcc make automake lrzsz -y
 
 yum install curses curses-devel ncurses ncurses-devel  -y
 rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
@@ -33,18 +33,7 @@ yum -y install ntp
 ntpdate pool.ntp.org
 systemctl start ntpd
 
-# 设置编码集
-yum -y groupinstall chinese-support
-yum -y install chinese-support
-yum -y install kde-l10n-Chinese
-yum -y install ibus-table-chinese-1.4.6-3.el7.noarch
 
-
-echo 'LANG="zh_CN.UTF-8"' > /etc/locale.conf
-echo 'LANG="zh_CN.UTF-8"' > /etc/sysconfig/i18n
-source /etc/locale.conf
-source /etc/sysconfig/i18n
-export LANG="zh_CN.UTF-8"
 
 # 安装 zsh oh-my-zsh 及插件，默认启动 zsh
 cd ~
@@ -57,9 +46,12 @@ cd ~
 
 sh -c "$(curl -fsSL https://gitee.com/caoayu/my-bash/raw/master/plugins/oh-my-zsh_install.sh)"
 
+export ZSH_CUSTOM="~/.oh-my-zsh/custom"
 
 git clone https://gitee.com/skarner/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 git clone https://gitee.com/Annihilater/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-echo "omz plugin enable docker docker-compose yum systemd rust python npm supervisor yarn pip pm2 node man aliases command-not-found history firewalld golang composer cp " >> ~/.zshrc
+echo "omz plugin enable docker docker-compose yum systemd rust python npm supervisor yarn pip pm2 node man aliases command-not-found history firewalld golang composer cp zsh-autosuggestions zsh-syntax-highlighting" >> ~/.zshrc
+
+exec zsh -l
