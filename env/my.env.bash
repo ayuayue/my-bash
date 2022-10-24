7a3b64bfa01867b6c2d3d9cf9802fe6d7683334a
ayuayue <1401262639@qq.com>
2022-10-24 13:53:39 +0800

    init

--- env/my.env.bash
+++ env/my.env.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # env
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
+# export LC_ALL=en_US.UTF-8
+# export LANG=en_US.UTF-8
 export LD_LIBRARY_PATH="/usr/local/instantclient"
 export APACHE="/usr/local/apache/bin"
 export PHP="/usr/local/php/bin"
---
+++ plugins/c_system_init.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env sh
+# Centos 系统初始化 
+# 安装 zsh oh-my-zsh 及插件，默认启动 zsh
+# 设置编码集
+# 设置 docker 源
+# 时间同步
+
+yum install glibc-common -y
+yum install -y langpacks-zh_CN
+
+echo 'export LC_ALL="zh_CN.UTF-8"' >> /etc/profile
+source /etc/profile
+echo 'LANG="zh_CN.UTF-8"' >> /etc/locale.conf
+source /etc/locale.conf
+
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors" : [
+    "https://cxlpw11m.mirror.aliyuncs.com",
+    "http://registry.docker-cn.com",
+    "http://docker.mirrors.ustc.edu.cn",
+    "http://hub-mirror.c.163.com"
+  ],
+  "insecure-registries" : [
+    "registry.docker-cn.com",
+    "docker.mirrors.ustc.edu.cn"
+  ],
+  "debug" : true,
+  "experimental" : true
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+cd ~
+wget https://jaist.dl.sourceforge.net/project/zsh/zsh/5.8/zsh-5.8.tar.xz
+tar xvf zsh-5.8.tar.xz
+cd zsh-5.8 && ./configure && make && make install
+echo /usr/local/bin/zsh >> /etc/shells
+chsh -s /usr/local/bin/zsh
+
+sh -c "$(curl -fsSL https://gitee.com/Devkings/oh_my_zsh_install/raw/master/install.sh)"
+
+git clone https://gitee.com/skarner/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+git clone https://gitee.com/Annihilater/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+yum install epel-release -y
+yum install yum-utils -y
+
+
+
+yum -y install ntp
+ntpdate pool.ntp.org
+systemctl start ntpd
+
+
+yum update
